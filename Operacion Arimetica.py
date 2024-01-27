829a4acb9d40f064048558f388715cc3e7ee9ecd
Jose Jimenez <75039091+losmin@users.noreply.github.com>
2024-01-26 19:25:47 -0600

    Add files via upload

---
+++ Operacion Arimetica.py
@@ -0,0 +1,2 @@
+resultado = (3 + 2 / 2 * 5) ** 2
+print("El resultado de la operación es:", resultado)
---
+++ calculo de paga.py
@@ -0,0 +1,5 @@
+horas_trabajadas = float(input("Ingrese el número de horas trabajadas: "))
+coste_por_hora = float(input("Ingrese el coste por hora: "))
+
+paga = horas_trabajadas * coste_por_hora
+print("La paga que le corresponde es:", paga)
